c4b4495b1bdf631838665a9c22052472aa537951
Kaio Gomes <kesteves.dev@gmail.com>
2025-06-22 12:49:05 -0300

    upd

---
+++ Migrations/20250609212844_create_tables1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinhaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_tables1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "photo",
+                schema: "user",
+                table: "user",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "photo",
+                schema: "user",
+                table: "user");
+        }
+    }
+}
---
+++ Migrations/20250609214204_create_tables132.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinhaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_tables132 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
